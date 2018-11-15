327a8dd25e5347a9f496fdb9250a768ec81eca93
Steven Spangler <steven@ikl.sh>
2018-11-14 20:26:46 -0500

    Add proper updating of existing rows and polish database class

--- app.py
+++ app.py
@@ -12,9 +12,12 @@ def input_form():
 @app.route('/', methods=['POST'])
 def input_form_post():
     db = database.Database("database_test")
+    db.set_match(request.form['matchnum'])
     db.set_team(request.form['team'])
+    db.set_self_values()
     db.set_number(request.form['number'])
     db.set_boolean(request.form['boolean'])
     db.set_string(request.form['string'])
+    db.commit()
     db.close()
-    return "probably added to the db but error codes are hard"
+    return "probably added to the db but error codes are hard" # TODO: actually error handle please
--- database.py
+++ database.py
@@ -2,7 +2,9 @@ import sqlite3
 
 
 class Database:
+
     def __init__(self, filename: str):
+        self.attributes = ["string", "number", "boolean"]
         self.match = 0
         self.team = 0
         self.string = "default"
@@ -10,16 +12,20 @@ class Database:
         self.boolean = 0
 
         self.filename = filename
-        self.connection = sqlite3.connect(filename + '.db')
+        self.connection = sqlite3.connect(self.get_filename())
         self.cursor = self.connection.cursor()
         self.cursor.execute('CREATE TABLE IF NOT EXISTS matches (matchnum, team, number, boolean, string)')
-        # self.cursor.execute('INSERT INTO teams VALUES (?,?,?,?)', (self.team, 0, False, 'Default'))
         self.connection.commit()
 
     def __set_def_values__(self):
-        if not self.check_stats_exist():
-            self.cursor.execute('INSERT INTO matches VALUES (?,?,?,?)', (self.team, 0, False, 'Default'))
-            self.commit()
+        if not self.__check_values_exist__():
+            self.cursor.execute('INSERT INTO matches VALUES (?,?,?,?,?)', (self.match, self.team, 0, False, 'Default'))
+
+    def __check_values_exist__(self):
+        self.cursor.execute("SELECT team FROM matches WHERE team = ? AND matchnum = ?", (self.team, self.match))
+        if self.cursor.fetchone() is None:
+            return False
+        return True
 
     def set_match(self, match: int):
         self.match = match
@@ -37,19 +43,23 @@ class Database:
         self.string = string
 
     def get_number(self):
-        self.cursor.execute('SELECT number FROM teams WHERE team=? AND matchnum=?', (self.team, self.match))
-        return self.cursor.fetchone()[0]
+        if not self.__check_values_exist__():
+            self.cursor.execute('SELECT number FROM matches WHERE team = ? AND matchnum = ?', (self.team, self.match))
+            return self.cursor.fetchone()[0]
 
     def get_filename(self):
         return self.filename+".db"
 
-    def check_stats_exist(self):
-        self.cursor.execute("SELECT team FROM teams WHERE team = ? AND matchnum=? ", (self.team, self.match))
-        return self.cursor.fetchone()
+    def set_self_values(self):
+        if self.__check_values_exist__():
+            self.cursor.execute("SELECT number, boolean, string FROM matches WHERE team = ? AND matchnum = ?", (self.team, self.match))
+            print(self.cursor.fetchone())
+            # self.number, self.boolean, self.string = self.cursor.fetchone() TODO: Fix, currently tries to set values even if none present
 
     def commit(self):
         self.__set_def_values__()
-        # self.cursor.execute('UPDATE ') # TODO: finish this
+        self.cursor.execute("UPDATE matches SET number = ?, boolean = ?, string = ? WHERE team = ? AND matchnum = ?", (self.number, self.boolean, self.string, self.team, self.match))
+        self.connection.commit()
 
     def close(self):
         self.connection.close()
